56084db435b283b61d7951a6ba5e73fb9f562674
Ashna-Vijili <78262288+Ashna-Vijili@users.noreply.github.com>
2021-02-01 23:50:52 -0800

    Update MergeEg.java

--- java programming/src/arrayprograms/MergeEg.java
+++ java programming/src/arrayprograms/MergeEg.java
@@ -7,12 +7,16 @@ import java.util.Arrays;
 	public static void main(String[] args)   
 	{  
 	int[] firstArray = {22,12,35,43,76,55,1};        
-	int[] secondArray = {41,32,66,99,1,5,7};  
+	int[] secondArray = {41,32,66,99,1,5,7}; 
+		
 	int f = firstArray.length;        
 	int s = secondArray.length;
-	int[] merge = new int[f + s];   
-	System.arraycopy(firstArray, 0, merge, 0, f);  
-	System.arraycopy(secondArray, 0, merge, f, s);  
+		
+	int[] merge = new int[f + s]; 
+		
+	       System.arraycopy(firstArray, 0, merge, 0, f);  
+		
+	      System.arraycopy(secondArray, 0, merge, f, s);  
 	System.out.println(Arrays.toString(merge));     
 	}  
 	}  
