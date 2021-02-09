d3c494e268245501370f8cb4c2ff10ec46f0aed0
Ashna-Vijili <78262288+Ashna-Vijili@users.noreply.github.com>
2021-02-09 09:15:53 -0800

    Aggregation and Encapsulation

---
+++ java programming/aggregationandencapsulation/TestCustomer.java
@@ -0,0 +1,18 @@
+package aggregationandencapsulation;
+
+public class TestCustomer {
+	
+	public static void main(String[] args) {
+		Address a=new Address("1st Main HSR Layout ," ,"Banglore");
+		Address a2=new Address("1st Main Electronics City, ","Banglore");
+		
+		Customer c=new Customer();
+		c.setCustomerName("John");
+		c.setResidentialAddress(a);
+		c.setOfficialAddress(a2);
+		
+		System.out.println("Customer name: "+c.getCustomerName() +"\nResidential Address : " +c.getResidentialAddress()+"\nOfficial Address : "+c.getOfficialAddress());
+		
+	}
+
+}
