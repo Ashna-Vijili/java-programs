5f9408ff61130a84bb86dc37cdb97f4d201f760c
Ashna-Vijili <78262288+Ashna-Vijili@users.noreply.github.com>
2021-02-09 03:39:19 -0800

    Add files via upload

---
+++ java programming/inheritanceandabstraction/Syrup.java
@@ -0,0 +1,16 @@
+package inheritanceandabstraction;
+
+public class Syrup extends Medicine {
+
+	public Syrup(double price, String expiry_date) {
+		super(price, expiry_date);
+		
+	}
+
+	@Override
+	void displayLabel() {
+		System.out.println("Shake well before use :");
+	}
+
+	
+}
---
+++ java programming/inheritanceandabstraction/Tablet.java
@@ -0,0 +1,17 @@
+package inheritanceandabstraction;
+
+public class Tablet extends Medicine{
+
+	public Tablet(double price, String expiry_date) {
+		super(price, expiry_date);
+		
+	}
+
+	@Override
+	void displayLabel() {
+		System.out.println("Store in a cool and dry place");
+		
+	}
+
+	
+}
