d9bb8e59fcd680665878fe77dd5d4668a86100f1
Ashna-Vijili <78262288+Ashna-Vijili@users.noreply.github.com>
2021-02-09 03:37:03 -0800

    OOPS assignment

---
+++ java programming/oopsassignment/TestCustomer.java
@@ -0,0 +1,17 @@
+package oopsassignment;
+
+public class TestCustomer {
+	
+	public static void main(String[] args) {
+		Address a=new Address("1st Main HSR Layout ," ,"Banglore");
+		
+		
+		Customer c=new Customer();
+		c.setCustomerName("John");
+		c.setResidentialAddress(a);
+		
+		System.out.println("Customer name: "+c.getCustomerName() +"\nResidential Address : " +c.getResidentialAddress());
+		
+	}
+
+}
