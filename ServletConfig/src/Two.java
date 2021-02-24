1c79b8d8a6734f1e9268e3c41d07b1154f3344a6
Ashna-Vijili <78262288+Ashna-Vijili@users.noreply.github.com>
2021-02-24 04:34:08 -0800

    Add files via upload

---
+++ ServletConfig/src/Two.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Two
+ */
+
+public class Two extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("this is two");
+		
+		ServletContext con=getServletContext();
+		String mydb=con.getInitParameter("database");
+		
+		out.print("<br>");
+		out.println("database is "+mydb);
+		
+		ServletConfig c=getServletConfig();
+		String message=c.getInitParameter("msg");
+		out.print("message is " +message +"<br>");
+	}
+
+}
---
+++ ServletContextDemo/src/Two.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Two
+ */
+@WebServlet("/Two")
+public class Two extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("this is two");
+		
+		ServletContext con=getServletContext();
+		String mydb=con.getInitParameter("database");
+		
+		out.print("<br>");
+		out.println("database is "+mydb);
+	}
+
+}
